e7f860f1b420aca0ca078b19c3d927e42b45dee3
globaltoken <admin@globaltoken.org>
2019-06-19 15:15:21 +0200

    Change to THROW_ERROR_EXCEPTION

--- argon2d/hashargon.cpp
+++ argon2d/hashargon.cpp
@@ -62,7 +62,7 @@ static void EnsureArgon2MemoryAllocated(const int64_t nTime)
             std::free (pArgon2Ad);
         pArgon2Ad = (uint8_t*) std::malloc(nSize);
         if (pArgon2Ad == nullptr)
-            throw std::runtime_error("Could not allocate memory for argon2");
+            throw THROW_ERROR_EXCEPTION("Could not allocate memory for argon2");
         nArgon2AdLen = nSize;
         UpdateArgon2AdValues();
     }
