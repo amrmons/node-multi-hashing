6a9eacb8e0d759415a658050790d6bdf958069d6
globaltoken <admin@globaltoken.org>
2019-06-22 17:18:35 +0200

    Removed extern c

--- x25x.h
+++ x25x.h
@@ -1,16 +1,6 @@
 #ifndef X25X_H
 #define X25X_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <stdint.h>
-
 void x25x_hash(const char* input, char* output);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
