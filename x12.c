f83635618af0e9e2b3ca6c65ddec8efa401e07bd
globaltoken <admin@globaltoken.org>
2019-06-22 21:10:54 +0200

    Wrong x12 implementation
    - Added new x12 implementation

--- x12.c
+++ x12.c
@@ -20,69 +20,69 @@
 
 void x12_hash(const char* input, char* output, uint32_t len)
 {
-    sph_blake512_context     ctx_blake;
-    sph_bmw512_context       ctx_bmw;
-    sph_groestl512_context   ctx_groestl;
-    sph_skein512_context     ctx_skein;
-    sph_jh512_context        ctx_jh;
-    sph_keccak512_context    ctx_keccak;
-    sph_luffa512_context    ctx_luffa1;
-    sph_cubehash512_context ctx_cubehash1;
-    sph_shavite512_context  ctx_shavite1;
-    sph_simd512_context     ctx_simd1;
-    sph_echo512_context     ctx_echo1;
-    sph_hamsi512_context    ctx_hamsi;
+    sph_blake512_context    ctx_blake;
+	sph_bmw512_context      ctx_bmw;
+	sph_luffa512_context    ctx_luffa;
+	sph_cubehash512_context ctx_cubehash;
+	sph_shavite512_context  ctx_shavite;
+	sph_simd512_context     ctx_simd;
+	sph_echo512_context     ctx_echo;
+	sph_groestl512_context  ctx_groestl;
+	sph_skein512_context    ctx_skein;
+	sph_jh512_context       ctx_jh;
+	sph_keccak512_context   ctx_keccak;
+	sph_hamsi512_context    ctx_hamsi;
 
     //these uint512 in the c++ source of the client are backed by an array of uint32
     uint32_t hashA[16], hashB[16];
 
-    sph_blake512_init(&ctx_blake);
-    sph_blake512 (&ctx_blake, input, len);
-    sph_blake512_close (&ctx_blake, hashA);
+	sph_blake512_init(&ctx_blake);
+	sph_blake512(&ctx_blake, input, 80);
+	sph_blake512_close(&ctx_blake, hashA);
 
-    sph_bmw512_init(&ctx_bmw);
-    sph_bmw512 (&ctx_bmw, hashA, 64);
-    sph_bmw512_close(&ctx_bmw, hashB);
+	sph_bmw512_init(&ctx_bmw);
+	sph_bmw512(&ctx_bmw, hashA, 64);
+	sph_bmw512_close(&ctx_bmw, hashB);
 
-    sph_groestl512_init(&ctx_groestl);
-    sph_groestl512 (&ctx_groestl, hashB, 64);
-    sph_groestl512_close(&ctx_groestl, hashA);
+	sph_luffa512_init(&ctx_luffa);
+	sph_luffa512(&ctx_luffa, hashB, 64);
+	sph_luffa512_close(&ctx_luffa, hashA);
 
-    sph_skein512_init(&ctx_skein);
-    sph_skein512 (&ctx_skein, hashA, 64);
-    sph_skein512_close (&ctx_skein, hashB);
+	sph_cubehash512_init(&ctx_cubehash);
+	sph_cubehash512(&ctx_cubehash, hashA, 64);
+	sph_cubehash512_close(&ctx_cubehash, hashB);
 
-    sph_jh512_init(&ctx_jh);
-    sph_jh512 (&ctx_jh, hashB, 64);
-    sph_jh512_close(&ctx_jh, hashA);
+	sph_shavite512_init(&ctx_shavite);
+	sph_shavite512(&ctx_shavite, hashB, 64);
+	sph_shavite512_close(&ctx_shavite, hashA);
 
-    sph_keccak512_init(&ctx_keccak);
-    sph_keccak512 (&ctx_keccak, hashA, 64);
-    sph_keccak512_close(&ctx_keccak, hashB);
+	sph_simd512_init(&ctx_simd);
+	sph_simd512(&ctx_simd, hashA, 64);
+	sph_simd512_close(&ctx_simd, hashB);
 
-    sph_luffa512_init (&ctx_luffa1);
-    sph_luffa512 (&ctx_luffa1, hashB, 64);
-    sph_luffa512_close (&ctx_luffa1, hashA);
+	sph_echo512_init(&ctx_echo);
+	sph_echo512(&ctx_echo, hashB, 64);
+	sph_echo512_close(&ctx_echo, hashA);
 
-    sph_cubehash512_init (&ctx_cubehash1);
-    sph_cubehash512 (&ctx_cubehash1, hashA, 64);
-    sph_cubehash512_close(&ctx_cubehash1, hashB);
+	sph_groestl512_init(&ctx_groestl);
+	sph_groestl512(&ctx_groestl, hashA, 64);
+	sph_groestl512_close(&ctx_groestl, hashB);
 
-    sph_shavite512_init (&ctx_shavite1);
-    sph_shavite512 (&ctx_shavite1, hashB, 64);
-    sph_shavite512_close(&ctx_shavite1, hashA);
+	sph_skein512_init(&ctx_skein);
+	sph_skein512(&ctx_skein, hashB, 64);
+	sph_skein512_close(&ctx_skein, hashA);
 
-    sph_simd512_init (&ctx_simd1);
-    sph_simd512 (&ctx_simd1, hashA, 64);
-    sph_simd512_close(&ctx_simd1, hashB);
+	sph_jh512_init(&ctx_jh);
+	sph_jh512(&ctx_jh, hashA, 64);
+	sph_jh512_close(&ctx_jh, hashB);
 
-    sph_echo512_init (&ctx_echo1);
-    sph_echo512 (&ctx_echo1, hashB, 64);
-    sph_echo512_close(&ctx_echo1, hashA);
+	sph_keccak512_init(&ctx_keccak);
+	sph_keccak512(&ctx_keccak, hashB, 64);
+	sph_keccak512_close(&ctx_keccak, hashA);
 
-    sph_hamsi512_init (&ctx_hamsi);
-    sph_hamsi512 (&ctx_hamsi, hashA, 64);
-    sph_hamsi512_close(&ctx_hamsi, hashB);
+	sph_hamsi512_init(&ctx_hamsi);
+	sph_hamsi512(&ctx_hamsi, hashA, 64);
+	sph_hamsi512_close(&ctx_hamsi, hashB);
 
     memcpy(output, hashB, 32);
 
